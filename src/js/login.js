cec535ebb21ba29121a0de05ef1df92c7a0ad1df
unluckypippo <filippoguerra1310@gmail.com>
2025-12-21 10:31:48 +0100

    fix: reso login più accessible

--- src/js/login.js
+++ src/js/login.js
@@ -4,8 +4,16 @@ const loginbtn = document.querySelector('.login-btn');
 
 regbtn.addEventListener('click', function(){
     container.classList.add('active');
+    const RegisterInput = document.querySelector('.register input');
+    if (RegisterInput) {
+        RegisterInput.focus();
+    }
 } )
 
 loginbtn.addEventListener('click', function(){
     container.classList.remove('active');
+    const LoginInput = document.querySelector('.login input');
+    if (LoginInput) {
+        LoginInput.focus();
+    }
 } )
