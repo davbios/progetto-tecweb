2794858827970658e813a9e4c91a0752de9064ae
Davide <tdavide@posteo.net>
2026-01-21 00:55:01 +0100

    feat: migliorata pagina index.php

--- src/index.php
+++ src/index.php
@@ -8,30 +8,17 @@ $template = str_replace("[keywords]", "drink, cocktails, alcolici, ricette, alco
 $template = str_replace("[navbar]", getNavbar("home", isset($_SESSION["user_id"])), $template);
 $template = str_replace("[breadcrumb]", '<span lang="en">Home</span>', $template);
 
-$content = '<section class="search-section">
-<h1>Cerca, scopri e perfeziona i migliori cocktail alcolici da tutto il mondo.</h1>
-<form action="cerca.html" method="GET">
-    <label for="site-search" class="visually-hidden">Cerca un drink</label>
-    <input type="search" id="site-search" name="q" placeholder="Hai già in mente un drink? Trovalo in un attimo." autocomplete="off">
-    <button type="submit" class="search-btn" aria-label="Avvia ricerca">
-        <span aria-hidden="true"><img src="img/search.svg" alt=""></span>
-    </button>
-</form>
-</section>
-<section>
-<h2>I <span lang="en">cocktail</span> più amati</h2>
-<ul class="drink-list">';
+$content = getTemplate("home");
 
-$drinks = $drinkDao->getAllOfficial(10, 0);
-foreach ($drinks as $drink) {
-    $drinkCard = file_get_contents(dirname(__FILE__) . "/templates/drink_card.html");
+$bestDrinksContent = "";
+foreach ($drinkDao->getAllOfficial(10, 0) as $drink) {
+    $drinkCard = getTemplate("drink_card");
     $drinkCard = str_replace("[drink]", $drink->name, $drinkCard);
     $drinkCard = str_replace("[image]", $drink->poster, $drinkCard);
     $drinkCard = str_replace("[id]", $drink->getId(), $drinkCard);
-    $content .= $drinkCard;
+    $bestDrinksContent .= $drinkCard;
 }
-$content .= '</ul>
-</section>';
+$content = str_replace("[best_drinks]", $bestDrinksContent, $content);
 
 $template = str_replace("[content]", $content, $template);
 
